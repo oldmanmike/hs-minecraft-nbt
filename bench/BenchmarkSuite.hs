b2b76522a6c509f622dc1892145fff7a0c9f567c
oldmanmike <oldmanmike.dev@gmail.com>
2016-06-26 23:40:40 -0400

    Start adding benchmarks for individual parsers

--- bench/BenchmarkSuite.hs
+++ bench/BenchmarkSuite.hs
@@ -2,6 +2,7 @@ import Criterion.Main
 import qualified  Data.Attoparsec.ByteString as Decode
 import qualified  Data.ByteString as B
 import qualified  Data.ByteString.Lazy as BL
+import qualified  Data.ByteString.Builder as Encode
 import qualified  Codec.Compression.GZip as GZip
 
 import Data.NBT.Decode
@@ -13,9 +14,82 @@ main = do
   rawLevel <- B.readFile "bench/benchdata/level.dat"
   let bigDat = BL.toStrict $ GZip.decompress (BL.fromStrict rawBig)
   let levelDat = BL.toStrict $ GZip.decompress (BL.fromStrict rawLevel)
-  defaultMain [
-    bgroup "decodeNBT"  [ bench "tiny" $ whnf (Decode.parseOnly decodeNBT) tinyDat
-                        , bench "big" $ whnf (Decode.parseOnly decodeNBT) bigDat
-                        , bench "level" $ whnf (Decode.parseOnly decodeNBT) levelDat
-                        ]
+  defaultMain
+    [ bgroup "decodeNBT"
+      [ bench "tiny" $ whnf (Decode.parseOnly decodeNBT) tinyDat
+      , bench "big" $ whnf (Decode.parseOnly decodeNBT) bigDat
+      , bench "level" $ whnf (Decode.parseOnly decodeNBT) levelDat
+      ]
+    , bgroup "decodeInt8"
+      [ bench "100" $ whnf (Decode.parseOnly decodeInt8) (B.replicate 100 0)
+      , bench "1000" $ whnf (Decode.parseOnly decodeInt8) (B.replicate 1000 0)
+      , bench "10000" $ whnf (Decode.parseOnly decodeInt8) (B.replicate 10000 0)
+      ]
+    , bgroup "decodeInt16BE"
+      [ bench "100" $ whnf (Decode.parseOnly decodeInt16BE) (B.replicate (100 * 2) 0)
+      , bench "1000" $ whnf (Decode.parseOnly decodeInt16BE) (B.replicate (1000 * 2) 0)
+      , bench "10000" $ whnf (Decode.parseOnly decodeInt16BE) (B.replicate (10000 * 2) 0)
+      ]
+    , bgroup "decodeInt32BE"
+      [ bench "100" $ whnf (Decode.parseOnly decodeInt32BE) (B.replicate (100 * 4) 0)
+      , bench "1000" $ whnf (Decode.parseOnly decodeInt32BE) (B.replicate (1000 * 4) 0)
+      , bench "10000" $ whnf (Decode.parseOnly decodeInt32BE) (B.replicate (10000 * 4) 0)
+      ]
+    , bgroup "decodeInt64BE"
+      [ bench "100" $ whnf (Decode.parseOnly decodeInt64BE) (B.replicate (100 * 8) 0)
+      , bench "1000" $ whnf (Decode.parseOnly decodeInt64BE) (B.replicate (1000 * 8) 0)
+      , bench "10000" $ whnf (Decode.parseOnly decodeInt64BE) (B.replicate (10000 * 8) 0)
+      ]
+    , bgroup "decodeWord16BE"
+      [ bench "100" $ whnf (Decode.parseOnly decodeWord16BE) (B.replicate (100 * 2) 0)
+      , bench "1000" $ whnf (Decode.parseOnly decodeWord16BE) (B.replicate (1000 * 2) 0)
+      , bench "10000" $ whnf (Decode.parseOnly decodeWord16BE) (B.replicate (10000 * 2) 0)
+      ]
+    , bgroup "decodeWord32BE"
+      [ bench "100" $ whnf (Decode.parseOnly decodeWord32BE) (B.replicate (100 * 4) 0)
+      , bench "1000" $ whnf (Decode.parseOnly decodeWord32BE) (B.replicate (1000 * 4) 0)
+      , bench "10000" $ whnf (Decode.parseOnly decodeWord32BE) (B.replicate (10000 * 4) 0)
+      ]
+    , bgroup "decodeWord64BE"
+      [ bench "100" $ whnf (Decode.parseOnly decodeWord64BE) (B.replicate (100 * 8) 0)
+      , bench "1000" $ whnf (Decode.parseOnly decodeWord64BE) (B.replicate (1000 * 8) 0)
+      , bench "10000" $ whnf (Decode.parseOnly decodeWord64BE) (B.replicate (10000 * 8) 0)
+      ]
+      {-
+    , bgroup "decodeFloatBE" []
+      [ bench "100" $ whnf (Decode.parseOnly decodeFloatBE) (B.replicateM Encode.int8
+      , bench "1000" $ whnf (Decode.parseOnly decodeFloatBE) (B.replicateM Encode.int8
+      , bench "10000" $ whnf (Decode.parseOnly decodeFloatBE) (B.replicateM Encode.int8
+      ]
+    , bgroup "decodeDoubleBE" []
+      [ bench "100" $ whnf (Decode.parseOnly decodeDoubleBE) (B.replicateM Encode.int8
+      , bench "1000" $ whnf (Decode.parseOnly decodeDoubleBE) (B.replicateM Encode.int8
+      , bench "10000" $ whnf (Decode.parseOnly decodeDoubleBE) (B.replicateM Encode.int8
+      ]
+    , bgroup "decodeText" []
+      [ bench "100" $ whnf (Decode.parseOnly decodeText) (B.replicateM Encode.int8
+      , bench "1000" $ whnf (Decode.parseOnly decodeText) (B.replicateM Encode.int8
+      , bench "10000" $ whnf (Decode.parseOnly decodeText) (B.replicateM Encode.int8
+      ]
+    , bgroup "decodeByteArray" []
+      [ bench "100" $ whnf (Decode.parseOnly decodeByteArray) (B.replicateM Encode.int8
+      , bench "1000" $ whnf (Decode.parseOnly decodeByteArray) (B.replicateM Encode.int8
+      , bench "10000" $ whnf (Decode.parseOnly decodeByteArray) (B.replicateM Encode.int8
+      ]
+    , bgroup "decodeList" []
+      [ bench "100" $ whnf (Decode.parseOnly decodeList) (B.replicateM Encode.int8
+      , bench "1000" $ whnf (Decode.parseOnly decodeList) (B.replicateM Encode.int8
+      , bench "10000" $ whnf (Decode.parseOnly decodeList) (B.replicateM Encode.int8
+      ]
+    , bgroup "decodeCompound" []
+      [ bench "100" $ whnf (Decode.parseOnly decodeCompound) (B.replicateM Encode.int8
+      , bench "1000" $ whnf (Decode.parseOnly decodeCompound) (B.replicateM Encode.int8
+      , bench "10000" $ whnf (Decode.parseOnly decodeCompound) (B.replicateM Encode.int8
+      ]
+    , bgroup "decodeIntArray" []
+      [ bench "100" $ whnf (Decode.parseOnly decodeIntArray) (B.replicateM Encode.int8
+      , bench "1000" $ whnf (Decode.parseOnly decodeIntArray) (B.replicateM Encode.int8
+      , bench "10000" $ whnf (Decode.parseOnly decodeIntArray) (B.replicateM Encode.int8
+      ]
+      -}
     ]
